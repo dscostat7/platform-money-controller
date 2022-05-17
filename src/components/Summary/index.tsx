dcbdb0c89a409ebb2ed62cb6095b02d461136d06
Diego Souza <diegosouzagestor@gmail.com>
2022-05-17 18:33:46 -0300

    Created components header application

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+import { Container } from "./styles";
+
+export function Summary() {
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={incomeImg} alt="Income SVG" />
+                </header>
+                <strong>R$1,000,00</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saídas</p>
+                    <img src={outcomeImg} alt="Outcome SVG" />
+                </header>
+                <strong>- R$500,00</strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="total SVG" />
+                </header>
+                <strong>R$500,00</strong>
+            </div>
+        </Container>
+    )
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,6 +7,7 @@ export const GlobalStyle = createGlobalStyle`
 
         --red: #e52e4d;
         --blue: #5429cc;
+        --green: #33cc95;
 
         --blue-light: #6933ff;
 

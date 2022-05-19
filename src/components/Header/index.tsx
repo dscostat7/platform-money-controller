241596e2027b5738d5e31047466700ea590d2212
Diego Souza <diegosouzagestor@gmail.com>
2022-05-19 19:10:16 -0300

    Create and install reactModal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,32 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content } from './styles';
 
 export function Header() {
+
+    const [isNewTransactionModalOpen, setIsNewTransaction] = useState(false);
+
+    function handleOpenTransactionModal() {
+        setIsNewTransaction(true);
+    }
+
+    function handleCloseTransactionModal() {
+        setIsNewTransaction(false);
+    }
+
     return (
         <Container>
             <Content>
                 
                 <img src={logoImg} alt="money controller" />
-                <button>
+                <button type="button" onClick={handleOpenTransactionModal}>
                     New transaction
                 </button>
+
+                <Modal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseTransactionModal}>
+                    <h2>Cadastrar transação!</h2>
+                </Modal>
             </Content>
         </Container>
     )

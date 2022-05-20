8f8e009de50ef822374f3ce6ba0328d3ffa94580
Diego Souza <diegosouzagestor@gmail.com>
2022-05-20 20:50:58 -0300

    styled modal new transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from "react-modal";
+import { Container } from "./styles";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -7,8 +8,24 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
     return (
-        <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-            <h2>Cadastrar transação!</h2>
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
+        >
+            <Container>
+                <h2>Cadastrar transação!</h2>
+
+                <input placeholder="Título" />
+                <input placeholder="Valor" type="number" />
+                <input placeholder="Categoria" />
+                
+                <button type="submit">
+                    Cadastrar
+                </button>
+            </Container>
+            
         </Modal>
     );
 }

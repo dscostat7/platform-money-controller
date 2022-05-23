18b4ea8ea42967db00103860ed37520b5f5c1d67
Diego Souza <diegosouzagestor@gmail.com>
2022-05-23 20:30:27 -0300

    format numbers and data

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -33,7 +33,25 @@ export function TransactionsTable() {
                 </thead>
 
                 <tbody>
-                    
+                    {transactions.map(transaction => {
+                        return (
+                            <tr key={ transaction.id }>
+                                <td>{ transaction.title }</td>
+                                <td className={transaction.type}>
+                                    {new Intl.NumberFormat('pt-BR', {
+                                        style: 'currency',
+                                        currency: 'BRL'
+                                    }).format(transaction.amount)}
+                                </td>
+                                <td>{ transaction.category }</td>
+                                <td>
+                                    {new Intl.DateTimeFormat('pt-BR').format(
+                                        new Date(transaction.createAt)
+                                    )}
+                                </td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
         </Container>

987bcf6504fb6526ffc9b0f5ced800f788bbab75
Diego Souza <diegosouzagestor@gmail.com>
2022-05-23 20:02:26 -0300

    configure route request

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,6 +4,7 @@ import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import closeImg from "../../assets/close.svg";
 import { Container, TransactionTypeContainer, RadioBox } from "./styles";
+import { api } from "../../services/api";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -20,7 +21,9 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        console.log(title, value, category, type);
+        const data = { title, value, category, type };
+        
+        api.post('/transactions', data);
     }
 
     return (

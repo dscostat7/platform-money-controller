358b565f70e59e2bf9d0a677c3ba4a352b2f9dd9
Diego Souza <diegosouzagestor@gmail.com>
2022-05-24 19:43:58 -0300

    ajust context

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -10,11 +10,27 @@ interface Transaction {
     createAt: string;
 }
 
+// interface TransactionInput {
+//     title: string;
+//     value: number;
+//     category: string;
+//     type: string;
+// }
+
+type TransactionInput = Omit<Transaction, 'id' | 'createAt'>;
+
 interface TransactionsProviderProps {
     children: ReactNode;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+    {} as TransactionsContextData
+);
 
 export function TransactionsProvider({children}: TransactionsProviderProps ){
     const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -24,8 +40,12 @@ export function TransactionsProvider({children}: TransactionsProviderProps ){
             .then(response => setTransactions(response.data.transactions))
     }, []);
 
+    function createTransaction(transaction: TransactionInput) {
+        api.post('/transactions', transaction);
+    }
+
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{ transactions, createTransaction }}>
             {children}
         </TransactionsContext.Provider>
     );
